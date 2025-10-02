6f10bc67a101ae8328dc1274976eaae93a7d062d
unknown <>
2025-10-02 20:22:35 +0200

    02/10/2025

--- Ejercicio1/respuesta.php
+++ Ejercicio1/respuesta.php
@@ -1,7 +1,14 @@
 <?php
-echo "<h2>".$_GET['nombre_usuario']."</h2>";
-echo "<h2>".$_GET['listaDeColores']."</h2>";
-if (.$_GET['listaDeColores']=='rojo'){
-    echo "<body style='background-color:red;'>"
+echo "<h2> El nombre del usuario " . $_GET['nombre_usuario'] . "</h2>";
+
+if ($_GET['listaDeColores'] == 'rojo') {
+    echo "<body style='background-color:red;'>";
+    echo "<h3> Eres palangana </h3>";
+} else if ($_GET['listaDeColores'] == 'verde') {
+    echo "<body style='background-color:#008000;'>";
+    echo "<h3> Eres del mejor equipo del mundo </h3>";
+} else if ($_GET['listaDeColores'] == 'otro') {
+    echo "<body style='background-color:red;'>";
+    echo "<h3>Eres palangana no me quieras mentir </h3>";
 }
 ?>
