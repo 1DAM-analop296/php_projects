d1a883c5d119b77fee39268dd2d2c4ef26781527
unknown <usuario@MVWinProx.iesmartinezm.es>
2025-10-24 14:50:58 +0200

    24/10/2025

--- Unidad2/FormularioYVariables/my-project/src/formulario.php
+++ Unidad2/FormularioYVariables/my-project/src/formulario.php
@@ -9,7 +9,8 @@
   </head>
   <body>
     <form action="respuesta.php" method="get">
-        <div class="m-3">
+        <div class="flex justify-center m-3 w-80 border p-3">
+          <div class="justify-center items-center">
             <!-- Para enviar le nombre  -->
         <label form="nombre">Nombre:</label>
         <input class="border" type="text" id="nombre" name="nombre_usuario">
@@ -18,21 +19,45 @@
          <!-- Para el apellido -->
         <label form="apellido">Apellido:</label>
         <input class="border" type="text" id="apellido" name="apellido_usuario">
-        
+        <br> <br>
         <!-- Para el correoElectronico -->
         <label form="correo">Correo:</label>
-        <input type="text" id="correo" name="correo_electronico">
-        
-        <label for="opciones">Dime el color que prefieras</label>
+        <input class="border" type="text" id="correo" name="correo_electronico">
+        <br> <br>
+        <label for="opciones">Dime tu País:</label>
         <select name="listaDeColores" id="opciones">
-            <option value="rojo"> Rojo</option>
-            <option value="verde">Verde</option>
-            <option value="otro"> Otro</option>
+            <option value="rojo"> España</option>
+            <option value="verde">Francia</option>
+            <option value="otro"> Portugal</option>
         </select>
         <br> <br>
-        <button type="submit">Enviar</button>
+         <label for="intereses">Selecciona tus intereses:</label><br>
+        <select id="intereses" class="form-select" multiple="true" name="hobbies[]">
+          <option value="programacion">Programación</option>
+          <option value="diseño">Diseño</option>
+          <option value="videojuegos">Videojuegos</option>
+          <option value="robotica">Robótica</option>
+        </select>
+        <br> <br>
+         <p>Selecciona tu curso:</p>
+         <input type="radio" id="curso1" name="curso" value="1DAM">
+         <label for="curso1">1DAM</label><br>
+
+         <input type="radio" id="curso2" name="curso" value="2DAM">
+         <label for="curso2">2DAM</label>
+        <br><br>
+        <div class="col-12">
+          <p>Dime una afición que tengas:<p>
+          <input type="checkbox" name="lenguajes[]" value="noticias">Noticias</input><br>
+          <input type="checkbox" name="lenguajes[]" value="promociones">Promociones</input><br>
+          <input type="checkbox" name="lenguajes[]" value="actualizaciones">Actualizaciones</input><br>
+          <input type="checkbox" name="lenguajes[]" value="eventos">Eventos</input><br>
         </div>
-    
+        <div class="flex justify-center">
+           <button class="justify-center border pl-10 pr-10  mt-4" type="submit">Enviar</button>
+        </div>
+        </div>
+    </div>
     </form>
   </body>
 </html>
