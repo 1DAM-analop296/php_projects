294e1426a8e795d48029abc2577368fc66e81d7c
unknown <usuario@MVWinProx.iesmartinezm.es>
2026-01-20 20:44:26 +0100

    20/01/2026

--- Unidad3/AGestorTareas/includes/header.php
+++ Unidad3/AGestorTareas/includes/header.php
@@ -10,12 +10,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <title>Kanban de tareas</title>
      <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <style>
-        .logo{
-            width: 5rem;
-            height: 5rem;
-        }
-    </style>
+    
 </head>
 
 <body>
@@ -23,14 +18,19 @@
         <div class="flex flex-column">
             <div class="flex flex-row items-center justify-center mb-5">
                 
-            <img src="../img/logo_tareas.png" class="img-fluid logo me-2" alt="Logo IES">
+            <img src="../img/logo_tareas.png" class="w-10" alt="Logo IES">
             <h1 class="titulo" class="text-xl font-bold">Kanban de tareas</h1>
             </div>
-            <div class="flex flex-row gap-6">
+             <div class="m-2">
+                <button type="button"class="flex !rounded-3xl justify-end items-center gap-2 bg-cyan-200  px-3 py-2">Añadir tarea
+                <img src="../img/mas.png" class="w-5 h-5 text-white">
+                </button>
+            </div>
+             <div class="flex flex-row gap-6"> 
                 <div class="w-full md:w-1/2 lg:w-1/3">
                     <p class="px-4 md:px-20 bg-red-600 text-white font-bold text-center mb-4 rounded-xl pt-2 pb-2">TO DO</p>
                     <?php
-                        require 'data.php';
+                        require_once 'data.php';
                         $tareas_toDo=[];
                         $resultado_tareas=getTareas($conn);
                         forEach($resultado_tareas as $tarea){
@@ -44,8 +44,15 @@
                             echo "<div class='flex flex-row w-full border-b-2 border-gray-400 justify-between mb-2 pb-1'>";
                                 echo "<p >".$tarea['fecha_entrega']."</p>";
                                 echo "<div class='flex flex-row gap-3'>";
-                                echo " <img src='../img/editar.png' class='w-5 h-5'>";
-                                echo " <img src='../img/papelera.png' class='w-5 h-5'>";
+                                echo" <form action='edit_tarea.php' method='post'>";
+                                echo  "<input type='hidden' name='tarea_id' value='<". $tarea['id'].">'>";
+                                    echo "<button class='items-center justify-center' class='btn'name='btn_modificar'>";
+                                        echo " <img src='../img/editar.png' class='w-5 h-5'/>";
+                                    echo "</button>";
+                                    echo "<button class='items-center justify-center' name='btn_eliminar'>";
+                                        echo " <img src='../img/papelera.png' class='w-5 h-5'/>";
+                                    echo "</button>";
+                                echo "</form>";
                                 echo "</div>";
                             echo "</div>";
                            
@@ -69,8 +76,15 @@
                          echo "<div class='flex flex-row w-full border-b-2 border-gray-400 justify-between mb-2 pb-1'>";
                                 echo "<p>".$tarea['fecha_entrega']."</p>";
                                 echo "<div class='flex flex-row gap-3'>";
-                                echo " <img src='../img/editar.png' class='w-5 h-5'>";
-                                echo " <img src='../img/papelera.png' class='w-5 h-5'>";
+                                 echo" <form action='edit_tarea.php' method='post'>";
+                                echo  "<input type='hidden' name='tarea_id' value='<?=". $tarea['id']." ?>'>";
+                                    echo "<button class='items-center justify-center' name='btn_modificar'>";
+                                        echo " <img src='../img/editar.png' class='w-5 h-5'/>";
+                                    echo "</button>";
+                                    echo "<button class='items-center justify-center' name='btn_eliminar'>";
+                                        echo " <img src='../img/papelera.png' class='w-5 h-5'/>";
+                                    echo "</button>";
+                                echo "</form>";
                                 echo "</div>";
                             echo "</div>";
                           echo"<p class='font-bold'>".$tarea['titulo']."</p>";
@@ -92,8 +106,15 @@
                            echo "<div class='flex flex-row w-full border-b-2 border-gray-400 justify-between mb-2 pb-1'>";
                                 echo "<p>".$tarea['fecha_entrega']."</p>";
                                 echo "<div class='flex flex-row gap-3'>";
-                                echo " <img src='../img/editar.png' class='w-5 h-5'>";
-                                echo " <img src='../img/papelera.png' class='w-5 h-5'>";
+                                 echo" <form action='edit_tarea.php' method='post'>";
+                                echo  "<input type='hidden' name='tarea_id' value='<?=". $tarea['id']." ?>'>";
+                                    echo "<button class='items-center justify-center' name='btn_modificar'>";
+                                        echo " <img src='../img/editar.png' class='w-5 h-5'/>";
+                                    echo "</button>";
+                                    echo "<button class='items-center justify-center' name='btn_eliminar'>";
+                                        echo " <img src='../img/papelera.png' class='w-5 h-5'/>";
+                                    echo "</button>";
+                                echo "</form>";
                                 echo "</div>";
                             echo "</div>";
                           echo"<p class='font-bold'>".$tarea['titulo']."</p>";
@@ -104,5 +125,6 @@
                 </div>
             </div>
         </div>
+</div>
     </header>
   
